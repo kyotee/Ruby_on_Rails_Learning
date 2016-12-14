d7a7649736c58623b03f36f84bd96c6c4171fa11
kyletam27 <kyle_tam1@hotmail.com>
2016-12-14 10:15:20 +0000

    Gravatar + Form handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,17 +26,18 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
@@ -68,8 +69,4 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
 end
