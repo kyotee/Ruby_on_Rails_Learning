f5f8c72510345414b1ee186c2888f3935b1569ec
kyletam27 <kyle_tam1@hotmail.com>
2016-12-21 07:38:08 +0000

    Cookies logout+concurrency

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,8 +30,14 @@ class User < ApplicationRecord
   end
   
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)                        
+  def authenticated?(remember_token)    
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
   
   
