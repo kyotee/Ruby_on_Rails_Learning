ea8c4f885de57034a5076c4a5375991e99d2c426
kyletam27 <kyle_tam1@hotmail.com>
2016-12-21 09:03:26 +0000

    rmbr me + chap 8 complete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,14 @@ module SessionsHelper
     end
   end
   
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
