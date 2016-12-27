9377fda859e92eb60834eadc6c33a189754b3b3d
kyletam27 <kyle_tam1@hotmail.com>
2016-12-27 07:37:22 +0000

    Reset edit and update actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,6 +70,12 @@ class User < ApplicationRecord
   end
   
   
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
+  
   
   private
   
